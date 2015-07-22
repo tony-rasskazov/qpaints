0c3682434d21fc0f5a41a3e20e5d63f513983363
Tony Rasskazov <tony.rasskazov@gmail.com>
2015-07-22 14:48:57 +0300

    Initial

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QPainter>
+#include <QTime>
+#include <QPen>
+#include <math.h>
+
+int rb = 40, rj = 30;
+
+int lrand(int low, int high) {
+  return qrand() % ((high + 1) - low) + low;
+}
+
+class Scene: public QGraphicsScene {
+public:
+    virtual void drawForeground(QPainter *painter, const QRectF & rect)
+    {
+        painter->translate(0.0, rect.height() / 2);
+        QPen pen;
+        pen.setWidth(7);
+        painter->setPen(pen);
+        drawBranch(painter, rect, 13);
+    }
+
+    void drawBranch(QPainter *p, const QRectF &rect, int depth) {
+        qreal h = rect.height();
+        if (depth > 0) {
+            p->drawLine(QPointF(0.0, 0.0), QPointF(0.0, -h * 0.15));
+            p->translate(0.0, -h * 0.15);
+            p->rotate(lrand(-25, 25));
+            if (lrand(0, 10) < 6)
+            {
+                p->scale(0.85, 0.85);
+                p->save(); p->rotate(lrand(rb - rj, rb + rj) ); drawBranch(p, rect, depth - 1); p->restore();
+                p->save(); p->rotate(lrand(-rb - rj, -rb + rj)); drawBranch(p, rect, depth - 1); p->restore();
+            } else { p->scale(0.7, 0.7); drawBranch(p, rect, depth); }
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    qsrand(QTime::currentTime().msec());
+    QApplication a(argc, argv);
+    Scene scene;
+    QGraphicsView view(&scene);
+    view.show();
+    return a.exec();
+}
---
+++ qpaints.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-03-31T02:48:53
+#
+#-------------------------------------------------
+
+QT       += core widgets
+
+#QT       -= gui
+
+TARGET = qpaints
+#CONFIG   += console
+#CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
